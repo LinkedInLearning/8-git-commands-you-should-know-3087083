44a0fbff6c5eb59c2183a7245b3b82270cdd2e25
Ronnie Sheer <ronysheer@gmail.com>
2022-09-19 14:58:14 +0000

    feat: add one to duration

--- docs/js/script.js
+++ docs/js/script.js
@@ -105,7 +105,7 @@ $(function () {
   new ScrollMagic.Scene({
     triggerElement: "#types",
     triggerHook: 0,
-    duration: 300,
+    duration: 3000001,
   })
     .setPin("#types")
     .setTween(typesTween)
