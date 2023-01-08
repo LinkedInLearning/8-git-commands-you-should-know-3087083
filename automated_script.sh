7ce36ec4afc7d92665653540592ebfb838dd4d4c
Haaris <89451392+haaris272k@users.noreply.github.com>
2023-01-08 06:25:20 +0000

    Modified

---
+++ automated_script.sh
@@ -0,0 +1,11 @@
+# Simple Script to automate the push of code to github
+
+git status
+git add -A
+git commit -m "Modified"
+git push
+echo "Done, status is "
+echo '---------------------------------------------'
+git status
+echo '---------------------------------------------''
+echo "That saved hell a lot of typing labour :)"
