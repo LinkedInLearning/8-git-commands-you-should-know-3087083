9d8a154f9efa34e9590fff68c09ad1ff4d3c2c64
Haaris <89451392+haaris272k@users.noreply.github.com>
2023-01-08 06:38:44 +0000

    modified script

--- automated_script.sh
+++ automated_script.sh
@@ -1,5 +1,6 @@
 # Simple Script to automate the push of code to github
-
+# Author @haaris272k
+# Note - The modification of all the files in the repo will be considered.
 git status
 git add -A
 git commit -m "Modified"
